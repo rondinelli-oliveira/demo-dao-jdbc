e8267fc35ef99b1ae911d3c5a810e6533b7b03d2
rondinelli-oliveira <rondinelliads@gmail.com>
2023-01-09 16:36:19 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DAOFactory;
+import model.dao.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,8 @@ public class Program {
 		Department department = new Department(1, "Books");
 
 		Seller seller = new Seller(1, "Rondinelli", "rondinelli@gmail.com", new Date(), 2900.0, department);
+		
+		SellerDAO sellerDAO = DAOFactory.createSellerDAO();
 
 		System.out.println(seller);
 
