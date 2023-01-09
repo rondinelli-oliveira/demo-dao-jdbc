bb475364162388846677addbf488cb405c8723fb
rondinelli-oliveira <rondinelliads@gmail.com>
2023-01-09 16:15:19 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Department department = new Department(1, "Books");
-		
-		System.out.println(department);
+
+		Seller seller = new Seller(1, "Rondinelli", "rondinelli@gmail.com", new Date(), 2900.0, department);
+
+		System.out.println(seller);
 
 	}
 
